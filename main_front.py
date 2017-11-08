fddc3c1159d3cbc393474af66c0568df738bd158
Vítor Finger <32837479+vitorfinger@users.noreply.github.com>
2017-11-08 08:23:21 -0200

    Inserção do arquivo de front-end
    Importado do Mac pessoal

---
+++ main_front.py
@@ -0,0 +1,28 @@
+from P05_Ponto_back import Database
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.widget import Widget
+
+database = Database("data.json")
+
+class MainScreen(Screen):
+    pass
+
+class CheckoutScreen(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+presentation = Builder.load_file("P05_Ponto_front_style.kv")
+
+class MainApp (App):
+    def build(self):
+        return presentation
+
+    def command_inserir_ponto(self, status):
+        database.inserir_ponto(status)
+
+if __name__ == "__main__":
+    MainApp().run()
